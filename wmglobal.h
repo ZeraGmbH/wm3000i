925d54112c029916dd2529240e746281fd512f7b
Peter Lohmer <peterlohmer01@gmail.com>
2019-11-05 13:42:15 +0100

    wmglobal.h: increased version number
    Signed-off-by: Peter Lohmer <peterlohmer01@gmail.com>

--- wmglobal.h
+++ wmglobal.h
@@ -90,9 +90,12 @@
 //                  die wm3000 blieb bis jetzt dann mit fehlermeldung unexpected answer stehen . busy wird jetzt recovert.
 //                  einbau berechnung rcf ratio correction factor. und grossanzeige um rcf erweitert. interface (measure)
 //                  erweitert. kontext menu ergänzt für die nachkommastellenzahl von rcf zu editieren.
+// v2.27 05.11.2019 neue kommandos für first asdu und last asdu und der automatismus aus dem bildschirmdialog
+//                  wurde übernommen dass wenn die anzahl der samples gesetzt wird ... dann ebenfalls die asdu's
+//                  mit gesetzt werden
 
 
-#define WMVersion "V2.26"
+#define WMVersion "V2.27"
 
 #define wm3000iHome QDir::homePath()
 // #define ServerCommLogFilePath "/usr/share/wm3000i/log/ServerComm.log"
